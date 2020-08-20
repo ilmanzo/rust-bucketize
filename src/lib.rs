c7c5c7db1141521a364f79f8de26a101a1daea2c
Andrea Manzini <ilmanzo@gmail.com>
2020-08-20 11:26:32 +0200

    second test

--- src/lib.rs
+++ src/lib.rs
@@ -32,4 +32,14 @@ mod tests {
         assert_eq!(bucketizer.bucketize(0.1),Some(0.5));
         assert_eq!(bucketizer.bucketize(999.999),None);
     }
+
+    #[test]
+    fn single_bucket_end_values() {
+        let mut bucketizer = Bucketizer::new();
+        bucketizer.add_bucket(Some(0.0), Some(1.0), 0.5);
+        assert_eq!(bucketizer.bucketize(0.0), Some(0.5));
+        assert_eq!(bucketizer.bucketize(1.0), None);
+    }
+
 }
+
